a705ea55fabc04eac9ef0f7395272018306e8bff
Aryan Srivastava <srivastavaaryan60@gmail.com>
2023-09-02 21:30:45 +0530

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name : 'Aryan Srivastava',
+        username : 'BOSS' ,
+        email : 'E22CSEU0604@bennett.edu.in' 
+    },
+    {
+        id: 2,
+        name : 'Ujjwal Singh',
+        username : 'Uj' ,
+        email : 'E22CSEU0616@bennett.edu.in' 
+    },
+    {
+        id: 3,
+        name : 'Shashwat Tripathi',
+        username : 'Shashi' ,
+        email : 'E22CSEU0587@bennett.edu.in' 
+    },
+    {
+        id: 4,
+        name : 'Aarushi Daksh',
+        username : 'Aru' ,
+        email : 'E22CSEU0619@bennett.edu.in' 
+    },
+    {
+        id: 5,
+        name : 'Pihu Prakash',
+        username : 'Moti' ,
+        email : 'E22CSEU1692@bennett.edu.in' 
+    },
+    {
+        id: 6,
+        name : 'Vivek Aggrwal',
+        username : '420op' ,
+        email : 'E22CSEU0610@bennett.edu.in' 
+    },
+    {
+        id: 7,
+        name : 'Madhav Garg',
+        username : 'Madhubala' ,
+        email : 'E22CSEU0602@bennett.edu.in' 
+    },
+    {
+        id: 8,
+        name : 'Hitanshu Singh',
+        username : 'Mera Camera Acha Hai' ,
+        email : 'E22CSEU0600@bennett.edu.in' 
+    },
+    {
+        id: 9,
+        name: 'Chinmay Siwach',
+        username : 'Chimnee',
+        email : 'E22CSEU0631@bennett.edu.in'
+    },
+    {
+        id : 10,
+        name : 'Utkarsh Tripathi',
+        username : 'Laundiyabaaz',
+        email : 'E22CSEU1501@bennett.edu.in'
+    }
+]
